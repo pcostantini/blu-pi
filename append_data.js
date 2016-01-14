d34f779e7a10f8d3853298c943a6f3b0a2c77ba4
Pablo Costantini <sixdemons@gmail.com>
2016-01-13 23:37:46 -0300

    # data appender

---
+++ append_data.js
@@ -0,0 +1,41 @@
+var SensorsDb = require('./main_persistence');
+
+var fs = require('fs');
+var _ = require('lodash');
+var Rx = require('rx');
+
+var dataDir = './data/';
+var mainDataFile = 'main.sqlite3';
+
+// Main Db where all sensor data is merged
+var mainDb = initMainDb();
+function initMainDb() {
+    var path = dataDir + mainDataFile;
+    return new SensorsDb(path);
+}
+
+// read from existing dbs
+var dbFetchs =
+    Rx.Observable.merge(
+        fs.readdirSync(dataDir)
+        .filter(function(s) { return _.last(s.split('.')) == 'sqlite3' })
+        .filter(function(s) { return s !== mainDataFile; })
+        .map(readFromDb))
+    .where(notEmpty)
+    .select(mainDb.insertSession)            // append to main db
+    // report
+    .mergeAll()
+    .do(console.log)
+    .filter(function(res) { return res.newSession })
+    .reduce(function(acc, current) { return acc + current; }, 0)
+    .subscribe(function(t) { console.log('added total rows:', t); });
+
+
+function readFromDb(dbFile) {
+    var db = new SensorsDb(dataDir + dbFile, true);
+    return db.readSensors();
+}
+
+function notEmpty(rowSet) {
+    return rowSet && rowSet.length > 0;
+}
