9096030dfeba387664310afdbc67d8bc85cf93aa
Pablo Costantini <sixdemons@gmail.com>
2016-04-09 17:38:57 -0300

    # fixes

--- app/display/distance.js
+++ app/display/distance.js
@@ -3,6 +3,7 @@ var GpsDistance = require('gps-distance');
 var refreshDisplayDelay = 1000;
 var width = 64;
 var height = 128;
+var speedThreshold = 5;
 
 function DistanceDisplay(driver, eventsStream, state) {
 
@@ -32,25 +33,27 @@ function DistanceDisplay(driver, eventsStream, state) {
 
         case 'Gps':
 
-          // distance
-          var coord = [s.value.latitude, s.value.longitude];
-          if(displayState.lastCoord) {
-            var offset = GpsDistance(
-              displayState.lastCoord[0], displayState.lastCoord[1],
-              coord[0], coord[1]);
-            displayState.distance += offset;
-          }
-          displayState.lastCoord = coord;
+          if(s.value) {
+            // distance
+            var coord = [s.value.latitude, s.value.longitude];
+            if(displayState.lastCoord) {
+              var offset = GpsDistance(
+                displayState.lastCoord[0], displayState.lastCoord[1],
+                coord[0], coord[1]);
+              displayState.distance += offset;
+            }
+            displayState.lastCoord = coord;
 
-          // speed
-          var speed = s.value ? s.value.speed : 0;
-          if(speed == undefined) speed = 0;
-          displayState.speed = mpsTokph(speed);
-  
-          // altitude
-          var altitude = s.value ? s.value.altitude : 0;
-          if(altitude == undefined) altitude = 0
-          displayState.altitude = altitude;
+            // speed
+            var speed = s.value ? s.value.speed : 0;
+            if(!speed || speed < speedThreshold) speed = 0;
+            displayState.speed = mpsTokph(speed);
+    
+            // altitude
+            var altitude = s.value ? s.value.altitude : 0;
+            if(altitude == undefined) altitude = 0
+            displayState.altitude = altitude;
+          }
 
           // draw!
           display(driver, displayState);
