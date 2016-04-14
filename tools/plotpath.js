a85c959c819454d365ac04fe6166e97258e15894
Pablo Costantini <sixdemons@gmail.com>
2016-04-14 10:03:57 -0300

    # tools updated

---
+++ tools/plotpath.js
@@ -0,0 +1,55 @@
+var Persistence = require('./persistence');
+var Rx = require('rx');
+var _ = require('lodash');
+
+var dbFilePath = process.argv[2];
+if(!dbFilePath) throw new Error('no path to .sqlite!');
+
+// load path
+var db = Persistence(dbFilePath, true);
+var pathDone = db
+  .readSensors()
+  .then(evts => evts.filter(isGps)
+                    .map(asTrackEvent)
+                    .filter(e => !!e.data)
+                    .map(getCoordinate));
+
+// inputs
+var inputs = require('./app/inputs_console')();
+inputs.subscribe(console.log);
+
+// ui
+var Driver = require('./app/display/drivers/oled');
+var GFX = require('edison-ssd1306/src/Adafruit_GFX');
+var width = 128;
+var height = 64;
+var driver = _.extend(
+  new GFX(height, width),     // invert size since oled is rotated
+  new Driver(width, height));
+
+
+pathDone.then(path => {
+  try {
+    var state = {
+      gpsPath: path
+    };
+    var Display = require('./app/display/map');
+    var ui = Display(driver, inputs, state);
+  } catch(err) {
+    console.log('INIT.ERR!', {
+      err: err,
+      stack: err.stack
+    });
+  }
+});
+
+// helpers
+const isGps = (s) => s.sensor === 'Gps' && !!s.data;
+
+const asTrackEvent = (s) => ({
+    timestamp: s.timestamp,
+    sensor: s.sensor,
+    data: JSON.parse(s.data)
+  });
+
+const getCoordinate = (s) => ([s.data.latitude, s.data.longitude]);
