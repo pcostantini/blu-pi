1f9ed8b391c056e4fe3633e9abd306c4d0954431
Pablo Costantini <sixdemons@gmail.com>
2016-05-08 07:33:58 +0000

    # set input gpios

--- app/inputs.js
+++ app/inputs.js
@@ -3,12 +3,12 @@ var gpio = require('./gpios');
 
 function GpioInputs() {
   
-  var inputNext = gpio.readPin(17, 0).map(() => ({ name: 'Input:Next' }));
-  // var inputBack = gpio.readPin(?, 0).map(() => ({ name: 'Input:Next' }));
-  // var inputOk = gpio.readPin(?, 0).map(() => ({ name: 'Input:Next' }));
+  var inputNext = gpio.readPin(16, 0).map(() => ({ name: 'Input:Next' }));
+  var inputBack = gpio.readPin(13, 0).map(() => ({ name: 'Input:Back' }));
+  var inputOk = gpio.readPin(12, 0).map(() => ({ name: 'Input:Ok' }));
 
   return Rx.Observable
-    .merge(inputNext/*, inputBack, inputOk*/)
+    .merge(inputNext, inputBack, inputOk)
     .share();
 }
 
