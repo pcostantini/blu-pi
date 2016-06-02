6868fc4656f0f82f46ad35a0fb84d1191b206ab0
Pablo Costantini <sixdemons@gmail.com>
2016-06-02 08:40:59 +0000

    # adjuster screen rotation to new layout

--- app/display/drivers/ssd1306.js
+++ app/display/drivers/ssd1306.js
@@ -198,7 +198,7 @@ OLED.prototype.drawPixel = function(x, y, color) {
 
   // check rotation, move pixel around if necessary
   //switch (getRotation()) {
-  switch (1) {
+  switch (3) {
   case 1:
     x = [y, y = x][0];//swap(x, y);
     x = SSD1306.LCDWIDTH - x - 1;
