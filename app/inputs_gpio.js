ac17ba77027f5ab29904dc70089e11565276e999
Pablo Costantini <sixdemons@gmail.com>
2016-06-24 21:02:10 +0000

    # cleanup

--- app/inputs_gpio.js
+++ app/inputs_gpio.js
@@ -28,20 +28,7 @@ function GpioInputs() {
         .filter(s => s.length > 0)
         .filter(s => s[0].value !== 'up')
         .filter(s => s.length > 0)
-        // .do(console.log)
         .map(s => ({ name: 'Input:' + (s[s.length-1].value === 'up' ? inputName : 'Long' + inputName) }))
-
-    // function checkType(evs) {
-    //   if (evs.length < 2) return inputName;
-    //   var delay = new Date().getTime() - evs[evs.length - 1].timestamp;
-    //   return (delay > longPressDelay ? 'Long' : '') + inputName;
-    // }
-
-    // return gpioObservable
-    //   .timestamp()                                                // add ts
-    //   .buffer(gpioObservable.filter(e => e.value === 'up'))        // take until botton is released
-    //   .map((evs) => ({ name: 'Input:' + checkType(evs) }));
-
   }
 
   // merge
