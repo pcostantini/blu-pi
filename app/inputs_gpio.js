3309d39dfa0c3f4ffab42747f9adf40e63516a8f
Pablo Costantini <sixdemons@gmail.com>
2016-06-24 20:48:58 +0000

    # refactored input short & long press

--- app/inputs_gpio.js
+++ app/inputs_gpio.js
@@ -7,12 +7,12 @@ function GpioInputs() {
   var gpioB = 17;
   var gpioC = 27;
 
-  var longPressDelay = 500;
+  var longPressDelay = 777;
 
   // REDEFINE:
-  // A: Cycle Prev
+  // A: Prev
   // B: Ok, LongOk
-  // C: Cycle Next
+  // C: Next
 
   var inputA = mapInput('A', gpio.observe(gpioA).share());
   var inputB = mapInput('B', gpio.observe(gpioB).share());
@@ -20,16 +20,27 @@ function GpioInputs() {
 
   function mapInput(inputName, gpioObservable) {
 
-    function checkType(evs) {
-      if (evs.length < 2) return inputName;
-      var delay = new Date().getTime() - evs[evs.length - 1].timestamp;
-      return (delay > longPressDelay ? 'Long' : '') + inputName;
-    }
+    var up = gpioObservable.filter(o => o.value === 'up');  // .skip(1)
+    var upOrSecond = (val) => Rx.Observable.merge(Rx.Observable.interval(longPressDelay), up);
 
     return gpioObservable
-      .timestamp()                                                // add ts
-      .buffer(gpioObservable.filter(e => e.value === 'up'))        // take until botton is released
-      .map((evs) => ({ name: 'Input:' + checkType(evs) }));
+        .bufferWhen(upOrSecond)
+        .filter(s => s.length > 0)
+        .filter(s => s[0].value !== 'up')
+        .filter(s => s.length > 0)
+        // .do(console.log)
+        .map(s => ({ name: 'Input:' + (s[s.length-1].value === 'up' ? inputName : 'Long' + inputName) }))
+
+    // function checkType(evs) {
+    //   if (evs.length < 2) return inputName;
+    //   var delay = new Date().getTime() - evs[evs.length - 1].timestamp;
+    //   return (delay > longPressDelay ? 'Long' : '') + inputName;
+    // }
+
+    // return gpioObservable
+    //   .timestamp()                                                // add ts
+    //   .buffer(gpioObservable.filter(e => e.value === 'up'))        // take until botton is released
+    //   .map((evs) => ({ name: 'Input:' + checkType(evs) }));
 
   }
 
