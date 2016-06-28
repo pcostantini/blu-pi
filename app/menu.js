82bad7945dd692c9592c1d4b30cf1489ecc8fd58
Pablo Costantini <sixdemons@gmail.com>
2016-06-28 11:51:55 +0000

    # menu - dim oled

--- app/menu.js
+++ app/menu.js
@@ -4,16 +4,24 @@ var exec = require('child_process').exec;
 function bash(cmd) {
   return () => {
     exec(cmd, (error, stdout, stderr) => {
-        if (error) {
-          console.log('exec error: ' + error);
-        }
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
+      if (error) {
+        console.log('exec error: ' + error);
+      }
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
     });
   }
 }
 
-var menu = [  
+var dimmed = false;
+var menu = [
+  {
+    name: 'dim oleds',
+    command: () => {
+      dimmed = !dimmed;
+      global.displayDriver.dim(dimmed);
+    }
+  },
   {
     name: 'kill leds',
     command: bash(
@@ -37,22 +45,27 @@ var menu = [
       'sudo ifup wlan0')
   }, {
     name: 'reboot',
-    command: bash('sudo reboot')
+    command: () => {
+      bash('sudo reboot')();
+      process.exit(0);
+    }
   }, {
     name: 'shutdown',
     command: bash('sudo shutdown -h -H -t 0 0')
   }, {
     name: 'tetris',
-    command: function() {
-      // ha!
+    command: function () {
+      var TetrisDisplay = require('./display/tetris.js');
+
+      global.displayEvents_generator.next({
+        type: 'change_display',
+        displayType: TetrisDisplay
+      })
     }
-  } 
+  }
 ];
 
-module.exports = menu.map(m => _.extend(
-  {
-    run: () => executeItem(m)
-  }, m));;
+module.exports = menu;
 
 function executeItem(menuItem) {
   console.log('running menuItem', menuItem);
