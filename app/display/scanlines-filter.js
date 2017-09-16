38826c11363905da2a826cbd6e6006029d8a3adb
Pablo Costantini <sixdemons@gmail.com>
2017-09-16 12:25:15 -0300

    # another mode

--- app/display/scanlines-filter.js
+++ app/display/scanlines-filter.js
@@ -3,8 +3,10 @@ module.change_code = 1;
 function Scanlines(driver, mode) {
 
   var scanlineMode = mode == 1
-    ? (x, y, color) => x % 2 === 1
-    : (x, y, color) => x % 2 === 1 || (x + y) % 2 === 0;
+    ? (x, y, color) => x % 2 === 1                            // 1
+    : (mode == 2)
+      ? (x, y, color) => y % 2 === 0                          // 2
+      : (x, y, color) => x % 2 === 1 || (x + y) % 2 === 0;    // 0
 
   driver._drawPixelScan = driver.drawPixel;
 
