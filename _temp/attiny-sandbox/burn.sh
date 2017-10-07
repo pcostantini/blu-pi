7b57ea186d47c9da9adfed213f9d2cf3518654d1
Pablo Costantini <sixdemons@gmail.com>
2017-10-06 23:21:36 -0300

    # burn.sh - set attiny85 to 8mhz

--- _temp/attiny-sandbox/burn.sh
+++ _temp/attiny-sandbox/burn.sh
@@ -1,7 +1,15 @@
+# set to 8mhz
 sudo gpio -g mode 22 out
 sudo gpio -g write 22 0
-# set to 8mhz
 sudo /usr/local/bin/avrdude -p t85 -P /dev/spidev0.0 -c linuxspi -b 10000 -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
+sudo gpio -g write 22 1
+
 # burn
+sudo gpio -g mode 22 out
+sudo gpio -g write 22 0
 sudo /usr/local/bin/avrdude -p t85 -P /dev/spidev0.0 -c linuxspi -b 10000 -U flash:w:attiny-sandbox.ino.tiny8.hex
 sudo gpio -g write 22 1
+
+
+
+
