b0e8adfb2f3e33704bde82b425a5cb06fbe27ea3
Pablo Costantini <sixdemons@gmail.com>
2017-10-06 22:58:55 -0300

    # burn.sh - set attiny85 to 8mhz

--- _temp/attiny-sandbox/burn.sh
+++ _temp/attiny-sandbox/burn.sh
@@ -1,8 +1,7 @@
-# compile
-# arduino --verify blink.ino --pref build.path=./build
-
-# burn
 sudo gpio -g mode 22 out
 sudo gpio -g write 22 0
+# set to 8mhz
+sudo /usr/local/bin/avrdude -p t85 -P /dev/spidev0.0 -c linuxspi -b 10000 -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
+# burn
 sudo /usr/local/bin/avrdude -p t85 -P /dev/spidev0.0 -c linuxspi -b 10000 -U flash:w:attiny-sandbox.ino.tiny8.hex
 sudo gpio -g write 22 1
