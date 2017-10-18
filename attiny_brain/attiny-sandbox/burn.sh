e95596f8f4202ab9c60944e01a5bed3c4900b47d
Pablo Costantini <sixdemons@gmail.com>
2017-10-18 01:56:08 -0300

    # attiny85 brain - speed sensor and odometer

--- attiny_brain/attiny-sandbox/burn.sh
+++ attiny_brain/attiny-sandbox/burn.sh
@@ -7,7 +7,7 @@ sudo gpio -g write 22 1
 # burn
 sudo gpio -g mode 22 out
 sudo gpio -g write 22 0
-sudo /usr/local/bin/avrdude -p t85 -P /dev/spidev0.0 -c linuxspi -b 10000 -U flash:w:attiny-sandbox.ino.tiny8.hex
+sudo /usr/local/bin/avrdude -p t85 -P /dev/spidev0.0 -c linuxspi -b 10000 -U flash:w:attiny_brain.ino.tiny8.hex
 sudo gpio -g write 22 1
 
 
