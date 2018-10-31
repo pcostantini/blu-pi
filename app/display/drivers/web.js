a16cf66b3bafbf43cb93afa00e3167e8a15b3423
Pablo Costantini <sixdemons@gmail.com>
2018-10-31 11:08:22 -0300

    # cleanup

--- app/display/drivers/web.js
+++ app/display/drivers/web.js
@@ -6,19 +6,73 @@ var SocketIo = require('socket.io');
 var httpPort = 8080;
 
 // Mock Display that routes buffer data using socket.io
-function OledMock(width, height) {
+function WebDisplay(width, height) {
 
-  this.bufferWidth =  width;
+  this.bufferWidth = width;
   this.bufferHeight = height;
   this.clear();
 
+  this.init();
+
+  console.log('\tWeb.Display.Driver ready @ ', httpPort);
+}
+
+WebDisplay.prototype.clear = function () {
+  this.buffer = [];
+  for (var y = 0; y < this.bufferHeight; y++) {
+    this.buffer[y] = _.fill(Array(this.bufferWidth), 0);
+  }
+}
+
+// update single pixel in buffer
+WebDisplay.prototype.drawPixel = function (x, y, color) {
+  if (!this.buffer) return;
+
+  x = Math.floor(x);
+  y = Math.floor(y);
+
+  // check rotation, move pixel around if necessary
+  ////switch (getRotation()) {
+  switch (3) {
+    case 1:
+      x = [y, y = x][0];//swap(x, y);
+      x = this.bufferWidth - x - 1;
+      break;
+    case 2:
+      x = this.bufferWidth - x - 1;
+      y = this.bufferHeight - y - 1;
+      break;
+    case 3:
+      x = [y, y = x][0];//swap(x, y);
+      y = this.bufferHeight - y - 1;
+      break;
+  }
+
+  if (x > this.bufferWidth - 1 || x < 0 ||
+    y > this.bufferHeight - 1 || y < 0) return;
+
+  var bit = !!color ? 1 : 0;
+  this.buffer[y][x] = bit;
+}
+
+// send buffer to socket for redraw
+WebDisplay.prototype.display = function () {
+  this.io.sockets.emit('buffer', this.buffer);
+}
+
+// not implemented
+WebDisplay.prototype.dim = function (dimmed) {
+  this.io.sockets.emit('dim', dimmed);
+}
+
+WebDisplay.prototype.init = function() {
   // web server to host page with canvas
   var server = http.createServer(function (req, res) {
     // index.html
     fs.readFile(__dirname + '/web.html', function (err, data) {
       if (err) {
-          res.writeHead(500);
-          return res.end('Error loading index.html');
+        res.writeHead(500);
+        return res.end('Error loading index.html');
       }
 
       // return page
@@ -35,62 +89,15 @@ function OledMock(width, height) {
     socket.emit('buffer', self.buffer);
 
     // recieve input events and broadcast them
-    socket.on('input', function(e) {
+    socket.on('input', function (e) {
       global.globalEvents_generator.next(e)
     });
 
   });
+
   this.io = io;
   server.listen(httpPort);
-
   this.inited = true;
 }
 
-OledMock.prototype.clear = function() {
-  this.buffer = [];
-  for(var y=0; y<this.bufferHeight; y++) {
-    this.buffer[y] = _.fill(Array(this.bufferWidth), 0);
-  }
-}
-
-// update single pixel in buffer
-OledMock.prototype.drawPixel = function(x, y, color) {
-  if(!this.buffer) return;
-
-  x = Math.floor(x);
-  y = Math.floor(y);
-
-  // check rotation, move pixel around if necessary
-  ////switch (getRotation()) {
-  switch (3) {
-  case 1:
-    x = [y, y = x][0];//swap(x, y);
-    x = this.bufferWidth - x - 1;
-    break;
-  case 2:
-    x = this.bufferWidth - x - 1;
-    y = this.bufferHeight - y - 1;
-    break;
-  case 3:
-    x = [y, y = x][0];//swap(x, y);
-    y = this.bufferHeight - y - 1;
-    break;
-  }
-
-  if(x > this.bufferWidth-1 || x < 0 ||
-     y > this.bufferHeight-1 || y < 0) return;
-
-  var bit = !!color ? 1 : 0;
-  this.buffer[y][x] = bit;
-}
-
-OledMock.prototype.display = function() {
-  // send buffer to socket for redraw
-  this.io.sockets.emit('buffer', this.buffer);
-}
-
-OledMock.prototype.dim = function(dimmed) {
-  // not implement
-}
-
-module.exports = OledMock;
+module.exports = WebDisplay;
