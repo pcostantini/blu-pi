685d13664dbb6f0a16f3751fb1e3515593771e18
Pablo Costantini <pablo.costantini@southworks.com>
2021-05-20 02:19:06 -0300

    # replay fix to avoid skipping non-gps events

--- app/replay_scheduled.js
+++ app/replay_scheduled.js
@@ -4,30 +4,28 @@ var _ = require('lodash');
 module.exports = function ReplayWithSchedule(sensors) {
 
     return Rx.Observable.create((observer) => {
-        console.log('scheduling...')
-
         var offset = 0;
-        var found = false;
-
-        sensors = sensors.skipWhile(o => {
-            if(found) {
-                return false;
-            }
-
-            found = o.name === 'Gps' && o.value && o.value.latitude;
-            return true;
-        });
-
-        sensors.first()
-            .subscribe(o => offset = o.timestamp);
+        sensors
+            .first()
+            .subscribe(o => {
+                offset = o.timestamp;
+                console.log('scheduling with offset:', offset);
+            });
 
         sensors
-            .map(o => _.assign({ offset: o.timestamp - offset }, o))
-            .subscribe(
-                o => Rx.Scheduler.async.schedule(() => {
+            // .map(o => _.assign({ offset: o.timestamp - offset }, o))
+            .subscribe(o => {
+                if(!offset) {
+                    console.log('skipping...?');
+                }
+
+
+                var newDelay = o.timestamp - offset;
+                Rx.Scheduler.async.schedule(() => {
                     var delayedO = _.assign({}, o, { timestamp: new Date().getTime() });
                     observer.next(delayedO);
-                }, o.offset));
+                }, newDelay);
+            });
 
     }).share();
 }
